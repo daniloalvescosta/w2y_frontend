28b75f1a9c697e3d6f8253c74058fef7dc227a6b
danilo costa <danilo.costa25932593@gmail.com>
2024-08-04 20:44:33 -0300

    added services for auth

---
+++ app/services/auth_service.rb
@@ -0,0 +1,54 @@
+class AuthService < ApplicationService
+
+  def self.login(email, password)
+    connection = Faraday.new(url: ENV.fetch('AUTH_URL', 'https://77f4-179-124-14-31.ngrok-free.app')) do |faraday|
+      faraday.request :json
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = connection.post('/login') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        email: email,
+        password: password
+      }.to_json
+    end
+
+    handle_response(response)
+  end
+
+  def self.register(email, password)
+    connection = Faraday.new(url: ENV.fetch('AUTH_URL', 'https://77f4-179-124-14-31.ngrok-free.app')) do |faraday|
+      faraday.request :json
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = connection.post('/register') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        "user": {
+          "email": email,
+          "password": password,
+          "password_confirmation": password
+        }
+      }.to_json
+    end
+
+    handle_response(response)
+  end
+
+  private
+
+  def self.handle_response(response)
+    case response.status
+    when 200..299
+      puts 'Login successful'
+      JSON.parse(response.body)
+    else
+      puts "Error: #{response.status} - #{response.body}"
+      nil
+    end
+  end
+end
