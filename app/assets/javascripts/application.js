872a268fb03726d7d3117c0f009714916b584686
danilo costa <danilo.costa25932593@gmail.com>
2024-08-04 22:56:26 -0300

    added styles for index show

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+// app/assets/javascripts/application.js
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
