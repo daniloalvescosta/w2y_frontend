5c71472cad83d4f951317e5d18044176f02062bb
danilo costa <danilo.costa25932593@gmail.com>
2024-08-04 22:55:11 -0300

    update dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,17 @@
 class DashboardController < ApplicationController
   include Authenticatable
 
-  def index; end
+  def index
+    @tasks = TasksService.get_tasks(session[:jwt])
+
+    if params[:status].present? && !params[:status].include?('all')
+      @tasks = @tasks.select { |task| params[:status].include?(task['status']) }
+    end
+  end
+
+  def show
+    @task = TasksService.get_task(session[:jwt], params[:uuid])
+  end
+
+  private
 end
